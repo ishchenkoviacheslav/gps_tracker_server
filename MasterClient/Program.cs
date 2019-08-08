4b9e23943c1d7ad03ef098c83c558ebe169d7195
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2019-08-08 21:06:18 +0300

    Add project files.

---
+++ MasterClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
